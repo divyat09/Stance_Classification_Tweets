4523df5d6af9245ad2b0a4eef7afd1939a6682ac
divyat09 <divyatcricket@gmail.com>
2016-06-28 18:32:37 +0530

    Updated with all attributes in Panda library

--- loading_corpus_2.py
+++ loading_corpus_2.py
@@ -4,5 +4,28 @@ import pandas as pd
 #data=pd.read_csv('/home/divyat/Desktop/RTE/Data/TrainingData/training_data_1.csv', delimiter="\t", quoting=3)
 data=pd.read_excel('/home/divyat/Desktop/RTE/Data/TrainingData/training_data_1.xlsx', index_col=None, na_values=['NA'] )
 
+#print all the columns
 print data.columns
+
+#print dimensions of data
 print data.shape
+
+#total number of rows
+print data.shape[0]
+
+#print the 3rd row: this commands a object of Data Type Series
+print data.loc[2]
+
+#print specific rows by passing a list as an argument 
+print data.loc[[1,10,19]]
+
+#print the column Target
+print data["Target"]
+
+#print some specific columns combined by passing a list containing name of those columns inside that list
+print data[["Target","Tweet"]]
+
+
+#print the data type of all the columns of data
+#print data.dtype
+
