9f2c0a0fac107845b6e506b1d381e97927f98131
divyat09 <divyatcricket@gmail.com>
2016-07-11 03:29:10 +0530

    Working Codegit add .

--- Main.py
+++ Main.py
@@ -1,5 +1,3 @@
-import nltk
-from nltk import word_tokenize
 import re
 import pandas as pd
 import sklearn
@@ -8,192 +6,233 @@ from sklearn import svm
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from collections import Counter
-from sklearn import svm
-import numpy
+import numpy as np
 import random
 
 
-'''
-f=open('/home/divyat/Desktop/RTE/Data/TrainingData/training_data_1.csv', 'rU')
-text=f.read()
-text1=text.split()
-abstracts=nltk.text(text1)
-'''
-
-'''
-#Making the matrix automatically using sklearn 
-vectorizer = CountVectorizer(lowercase=True, stop_words="english")
-matrix = vectorizer.fit_transform(list_tweets)
-print matrix.shape
-'''
-
-#Calculate the count of word specificially when it occurs with the Target
-def count_word_target(word,target):
-	count=0
-	index=[]	
-
-	#Taking a subset from data so that only those rows are taken which have target in Local Target column
-	for item in data['Local Target']:
-		item=str(item).lower().replace(" ","")
-		index.append(target in item)
-	data_sub=data[index] 
-
-	#Making list of all tweets_sub 
-	tweets_sub=[]
-	for item in data_sub['Tweet']:
-		tweets_sub.append(str(item))
-	# Replace sequences of whitespace and lowercase
-	tweets_sub = [re.sub("\s", "", t.lower()) for t in tweets_sub]
-	
-
-	#No of times the word occurs in Tweet column of new data set where all values in Local Target column contain target	
-	for item in tweets_sub:
-		if  word in item:			
-			count=count+1	
-	
-	#print target,"\t",count,"\n"	
-	return count
-
-#Calculate the PMI of a knowledge source and determine if it to be selected for the feature set
-def pmi(word):
-	count=0
-	pmi_word_t1=0
-	pmi_word_t2=0
-	pmi_word_t3=0
-	pmi_word_t4=0
-	pmi_word=0
-
-    #Total count of word in the Tweet column of data
-	for item in tweets:
-		if  word in item:
-			count=count+1
-	#print word,"\t",count,"\n","\n"	
-
-	target='babyrights'	
-	if count!=0:	
-		pmi_word_t1=count_word_target(word,target)/float(count)
-	
-	target='womenrights'	
-	if count!=0:	
-		pmi_word_t2=count_word_target(word,target)/float(count)
-
-	target='christianity'	
-	if count!=0:	
-		pmi_word_t3=count_word_target(word,target)/float(count)
-
-	target='adoption'	
-	if count!=0:	
-		pmi_word_t4=count_word_target(word,target)/float(count)		
-	
-	pmi_word=max(pmi_word_t1,pmi_word_t2,pmi_word_t3,pmi_word_t4)
-
-	if pmi_word==pmi_word_t1 and pmi_word>= 0.85 and count>=3 :
-		return word	
-		
-	if pmi_word==pmi_word_t2 and pmi_word>= 0.85 and count>=2:
-		return word
-		
-	if pmi_word==pmi_word_t3 and pmi_word>= 0.85 :
-		return word
-		
-	if pmi_word==pmi_word_t4 and pmi_word>= 0.85 and count>=3 :
-		return word		
-					
-	return 0
-
-#Build the Feature Set from List of Knowledege Sources
-def Features():
-
-	feature_set=[]
-
-	list_words_1=['Medical Care','Liberal','liberalism','Women Health','Health','Profeminist', 'Prochoice','Bodily Autonomy', 'Rapeculture','Rape','Win for women','ReproductiveRights','Reproductive Health','Feminist','Feminism','ReproRights','Right','choose', 'Pregnancy']
-	list_words_2=['promarriage','Mother Teresa','Death Penalty','Baby','Child','Murder','Kill', 'ProLife' , 'Promurder', 'ProLifeYouth','ProLifeGen','Probaby', 'Protect Voiceless' , 'Life','Protect Life', 'Legalised Murder', 'Human Life', 'Hitler','Humanity', 'Unborn','Voiceless','Human','Innocent']
-	list_words_3=['God','atheism','atheist','Catholic','Religious','Jesus','Bible','Christian','Church','Prayer']
-	list_words_4=['Fostering','Adoption','Adopt']
-	list_hashtag_1=['#SCOTUS','#Feminism','#Rapeculture', '#WomensRights', '#ReproductiveRights', '#MyBodyMyRights', '#Reprojustice','#womensrights', '#Reprorights', '#Rape','#prochoice' '#YourBody', '#AbortionRight','#womenshealth','#prochoice', '#reprohealth',"#RightToChoose"]
-	list_hashtag_2=['#murder', '#voiceless', '#AbortionIsMurder', '#prolife', '#ProLifeYouth','#ProLifeGen', '#killing', '#child', '#Life', '#Mother', '#InnocentLives', '#ProtectLife', '#LifeIsBeautiful', '#AllLivesMatter', '#BlackLivesMatter', '#LifeWins' , '#Unborn' , '#MotherTeresa', '#ISIS']
-	list_hashtag_3=['#jesus', '#bible', '#God','#Catholic', '#Christian','Christianity']
-	list_hashtag_4=['#adopt','#adoption']
-	
-	list_words=list_words_1+list_words_2+list_words_3+list_words_4
-	list_hashtags=list_hashtag_1+list_hashtag_2+list_hashtag_3+list_hashtag_4
-	list_total=list_words+list_hashtags
-	# Replace sequences of whitespace and Lowercase
-	list_total = [re.sub("\s", "", t.lower()) for t in list_total]
-	
-	for item in list_total:
-		word=pmi(item)
-		if word!=0:
-			feature_set.append(word)
-
-	return feature_set
-
-
-def make_matrix(tweets, vocab):
+def make_matrix(tweets, features):
     matrix = []
     row=[]
     for tweet in tweets:
-        for w in vocab:
-        	row.append(tweet.count(w))	
+        for feature in features:
+        	#Each element of row is count of feaure in tweet
+        	row.append(tweet.count(feature))	
         matrix.append(row)
-        print row
         row=[]
-       
 
     df = pd.DataFrame(matrix)
-    df.columns = vocab
+    df.columns = features
+    
     return df
 
 
+def Label(df,target):
+	label=[]
+	for item in df[target]:
+		label.append(str(item))
+	return label
+		
+def Tweets(df):
+	tweet=[]
+	for item in df['Tweet']:
+		tweet.append(str(item))
+
+	removelist="#"
+	# Lowercase, then replace any non-letter, space, or digit character in the headlines except Hashtag
+	tweet = [re.sub(r'[^\w\s\d'+removelist+']','',t.lower()) for t in tweet]
+	# Replace sequences of whitespace with a space character.
+	tweet = [re.sub("\s+", " ", t) for t in tweet]
+	# Replace all spaces in string
+	tweet = [re.sub(" ", "", t) for t in tweet]
+
+	return tweet
+
+def Features(target):
+	
+	f=open('/home/divyat/Desktop/RTE/Data/TrainingData/'+target+'.txt','r')
+	feature_set=f.read().split("\n")
+	feature=[]
+	for item in feature_set:
+		feature.append(item)
+	print len(feature)
+	f.close()
+
+	return feature
+
+def Analysis(clf,matrix,label,target,values):
+	
+	index=0
+	error=0
+	count=0
+	new_count=0
+	for item in clf.predict(matrix):
+		if item!=label[index]:
+			error=error+1
+		index=index+1
+
+	f=open('/home/divyat/Desktop/RTE/Data/TrainingData/Analysis.txt','a')
+
+	f.write('Analysis for '+target+'\n'+'\n')
+	for item in values:
+		for t in label:
+			if t==item:
+				count=count+1
+		for t in clf.predict(matrix):
+			if t==item:
+				new_count=new_count+1
+		f.write('Old Count of'+item+': '+str(count)+'\n')
+		f.write('New Count of'+item+': '+str(count)+'\n'+'\n')
+		count=0
+		new_count=0
+
+	f.write('Total Wrong Cases: '+ str(error)+"\n")
+	f.write('Accuracy: '+ str(clf.score(matrix,label,sample_weight=None)) +'\n'+'\n')
+	f.close()
+
+def train_svm_stance(target,sec_target):
+
+	#If No element in data frame with specified secondary target
+	if not target in sec_target:
+		return 0
+	
+	sub_data=data[target==sec_target]
+
+	#Making list of all tweets 
+	sub_tweets=Tweets(sub_data)
+
+	#Feature Set made by selecting words with high pmi from all the words form tweets
+	sub_features =Features(target)
+	
+	#Convert the tweets into feature vectors and make a matrix of feature vectors
+	sub_matrix=make_matrix(sub_tweets,sub_features)
+	
+	#Mark the classes of training set tweets for training the SVM
+	sub_label=Label(sub_data,'Stance')
+
+	#Implementing the Support Vector Machine
+	clf = svm.SVC(kernel='linear')
+	print clf
+	
+	clf.fit(sub_matrix, sub_label) 
+	
+	#Analysis on Trained SVM
+	Analysis(clf,sub_matrix,sub_label,target,['FAVOR','AGAINST','NONE'])
+
+	return clf
+
+def train_svm(tweets,features):
+	
+	'''			
+	#Adding some meta features
+	transform_functions= [
+	    lambda x: len(x),
+	    lambda x: x.count(" "),
+	    lambda x: x.count("."),
+	    lambda x: x.count("!"),
+	    lambda x: x.count("?"),
+	    lambda x: len(x) / (x.count(" ") + 1),
+	    lambda x: x.count(" ") / (x.count(".") + 1),
+	    lambda x: len(re.findall("\d", x)),
+	    lambda x: len(re.findall("[A-Z]", x)),
+	]
+	columns = []
+	for func in transform_functions:
+	    columns.append(data["Tweet"].apply(func))
+
+	meta = numpy.asarray(columns)
+	'''
+
+	#Convert the tweets into feature vectors and make a matrix of feature vectors
+	matrix=make_matrix(tweets,features)
+
+	#Mark the classes of training set tweets for training the SVM
+	label=Label(data,'Local Target')
+	
+	#Implementing the Support Vector Machine
+	clf = svm.SVC(kernel='linear')
+	print clf
+	clf.fit(matrix, label)
+
+	
+	#Train the Stance Classifier for each of the subsets we have made using Secondary Targets 
+	clf1=train_svm_stance('Baby Rights',clf.predict(matrix))
+	clf2=train_svm_stance('Women Rights',clf.predict(matrix))
+	#clf3=train_svm_stance('Christianity',clf.predict(matrix))
+	clf4=train_svm_stance('Adoption',clf.predict(matrix))
+	clf5=train_svm_stance('Abortion',clf.predict(matrix))
+	clf6=train_svm_stance('Other',clf.predict(matrix))
+
+	classifiers =[]
+	classifiers.append(clf)
+	classifiers.append(clf1)
+	classifiers.append(clf2)
+	#classifiers.append(clf3)
+	classifiers.append(clf4)
+	classifiers.append(clf5)
+	classifiers.append(clf6)
+	
+	for item in classifiers:
+		if item==0:
+			classifiers.remove(item)
+
+	#Analysis on Trained SVM
+	Analysis(clf,matrix,label,'Secondary Targets',['Baby Rights','Women Rights','Christianity','Adoption','Abortion','Other'])
+	
+	return classifiers
+
 #Main Code Starts Here
 
-
-data=pd.read_excel('/home/divyat/Desktop/RTE/Data/TrainingData/Training_Set.xlsx', index_col=None, na_values=['NA'] )
+data=pd.read_excel('/home/divyat/Desktop/RTE/Data/TrainingData/Training_Set_New.xlsx', index_col=None, na_values=['NA'] )
+data_test=pd.read_excel('/home/divyat/Desktop/RTE/Data/TestData/test_data.xlsx', index_col=None, na_values=['NA'] )
 
 #Making list of all tweets 
-tweets=[]
-for item in data['Tweet']:
-	tweets.append(str(item))
-# Replace sequences of whitespace with a space character.
-tweets = [re.sub("\s", "", t.lower()) for t in tweets]
+tweets=Tweets(data)
 
-#Feature Set
-'''
-features_words=['bodilyautonomy','rapeculture','reproductiverights','reprorights','murder','prolifegen','humanlife','unborn','atheism','adopt']
-features_hashtags=['#feminism','#rapeculture','#reproductiverights','#reprorights','#rape','#abortionright','#murder','#prolifegen','#child','#life','#alllivesmatter','#unborn','#motherteresa','#god','#adopt']
-features=features_words+features_hashtags
-'''
-features=Features()
-matrix=make_matrix(tweets,features)
+#Feature Set made by selecting words with high pmi from all the words form tweets
+features = Features('features')
 
-label=[]
-for item in data['Local Target']:
-	label.append(str(item))
+#Train SVM classifier to classify tweets into secondary targets
+classifiers=train_svm(tweets, features)
 
-transform_functions= [
-    lambda x: len(x),
-    lambda x: x.count(" "),
-    lambda x: x.count("."),
-    lambda x: x.count("!"),
-    lambda x: x.count("?"),
-    lambda x: len(x) / (x.count(" ") + 1),
-    lambda x: x.count(" ") / (x.count(".") + 1),
-    lambda x: len(re.findall("\d", x)),
-    lambda x: len(re.findall("[A-Z]", x)),
-]
-columns = []
-for func in transform_functions:
-    columns.append(data["Tweet"].apply(func))
+#Making list of all tweets in test set
+test_tweets=Tweets(data_test)
 
-meta = numpy.asarray(columns)
+#Convert the tweets into feature vectors and make a matrix of feature vectors which  serve as an input to classifier trained on Training Data
+matrix=make_matrix(test_tweets,features)
 
-#print matrix.shape
-#print meta.shape 
+#Predicting Stance for the Test Data 
+stance_predicted=[]
+list_target=['Baby Rights','Women Rights','Christianitydsadasdasd','Adoption','Abortion','Other']
 
-clf = svm.SVC()
-clf.fit(matrix, label) 
-index=0
+#Secondary Target Classification of Test Tweets
+target_predicted=classifiers[0].predict(matrix)
 
-for item in clf.predict(matrix):
-	print item
+for item in list_target:
+	if not item in target_predicted:
+		list_target.remove(item)
 
+for i in range(0,len(classifiers)-1):
+	#Making new data test specific to a secondary target
+	new_data=data_test[list_target[i]==target_predicted]
+	
+	#Making list of all tweets specific to a secondary category
+	new_tweets=Tweets(new_data)
+	
+	#Making list of features specific to a secondary category
+	new_features=Features(list_target[i])
+
+	#Making matrix specific to a secondary target
+	new_matrix=make_matrix(new_tweets,new_features)
+	
+	stance_predicted=stance_predicted + classifiers[i+1].predict(new_matrix).tolist()
+
+#Adding the prediced data to the Test Set
+data_test['Target_Predicted'] = pd.Series(target_predicted, index=data_test.index)
+data_test['Stance_Predicted'] = pd.Series(np.asarray(stance_predicted), index=data_test.index)
+
+# Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter('/home/divyat/Desktop/RTE/Data/TestData/test_data.xlsx', engine='xlsxwriter')
+# Convert the dataframe to an XlsxWriter Excel object.
+data_test.to_excel(writer, sheet_name='Sheet1')
+# Close the Pandas Excel writer and output the Excel file.
+writer.save()
